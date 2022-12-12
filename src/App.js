ab13cd444ac171f87d49b69be5506e938f091ad5
Anushka-Gamad <anushkagamad553@gmail.com>
2022-12-12 18:27:29 +0530

    testimonials section complete

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import NavMobile from './components/NavMobile';
 import Experience from './components/Experience';
 import Video from './components/Video';
 import Headsets from './components/Headsets';
+import Testimonial from './components/Testimonial';
 
 
 const App = () => {
@@ -27,6 +28,7 @@ const App = () => {
       <Experience />
       <Video />
       <Headsets />
+      <Testimonial />
     </div>
   );
 };
--- src/components/Testimonial.js
+++ src/components/Testimonial.js
@@ -1,7 +1,30 @@
 import React from 'react';
 
+
+//import components
+import TestimonialSlider from './TestimonialSlider';
+
+
 const Testimonial = () => {
-  return <div>Testimonial</div>;
+  return (
+    <section className='mb-64 lg:mb-48'>
+      <div className='container mx-auto'>
+        <div className='bg-purple-400/10 max-h-[400px]
+        p-12 rounded-lg'>
+          <div className='text-center'>
+            <h2 className='text-3xl font-bold mb-6'>What our clients say</h2>
+            <p className='max-w-2xl mx-auto mb-12
+            lg:mb-24'>Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.
+            </p>
+          </div>
+          {/* slider */}
+          <div >
+            <TestimonialSlider />
+          </div>
+        </div>
+      </div>
+    </section>
+    );
 };
 
 export default Testimonial;
